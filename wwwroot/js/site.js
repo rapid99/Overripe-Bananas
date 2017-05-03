72d801f14db7fbc93e158c9f71982d80d504f2d9
Hyman <mhyman@hosp.dhha.org>
2017-05-03 15:40:46 -0600

    overall grade is now calculated on submit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿(function ($) {
-
-    
-(jQuery));
+﻿$(document).ready(function () {
+    $("#createForm").on("submit", function () {
+        var humor = document.getElementById("humorRating").value;
+        var story = document.getElementById('storyRating').value;
+        var sum = parseInt(humor) + parseInt(story);
+        console.log(sum);
+    });
+});
